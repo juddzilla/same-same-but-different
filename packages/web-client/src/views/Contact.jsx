b7671c9eac31d4448916298863a8178840be4346
Judd Hendrix <juddhendrix@gmail.com>
2023-10-26 11:01:56 -0700

    contact

--- packages/web-client/src/views/Contact.jsx
+++ packages/web-client/src/views/Contact.jsx
@@ -1,6 +1,23 @@
+const style = {
+  alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+const container = { maxWidth: '80ch' };
+
 const Component = () => {
   return (
-      <div>Contact</div>
+      <div>
+        <h1 className='headline'>Privacy Policy</h1>
+        <div className='view-content' style={style}>
+          <div style={container}>
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLScnamev-h0Fb1CK9Uwo7UnPbZoe2twVnvTExCG4zWNS0yzJmg/viewform?embedded=true"
+                width="640" height="1000" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+          </div>
+        </div>
+      </div>
   );
 };
 
@@ -8,3 +25,4 @@ export default {
   element: <Component />,
   path: '/contact',
 };
+
